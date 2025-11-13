f0b4fef691b6de979bde6d53d23008d57b72688b
PeuMAndrade <peumandrade@gmail.com>
2025-11-13 18:54:13 -0300

    feat: Integra o WiFiManager para configuração dinâmica de Wi-Fi

--- include/wifi_manager.h
+++ include/wifi_manager.h
@@ -1,19 +1,8 @@
-/**
- * @file wifi_manager.h
- * @brief Cabeçalho do gerenciador de conexão Wi-Fi e sincronização de tempo (NTP).
- * @details Este arquivo define a interface para todas as funcionalidades relacionadas à rede Wi-Fi no ESP32.
- *          Ele abstrai a complexidade da biblioteca WiFi.h do Arduino, fornecendo funções de alto nível para
- *          conectar, reconectar e verificar o status da conexão. Além disso, inclui a funcionalidade para
- *          sincronizar o relógio interno do dispositivo com um servidor de tempo da internet (NTP), o que é
- *          essencial para a precisão dos timestamps em logs e outras operações sensíveis ao tempo.
- */
 #ifndef WIFI_MANAGER_H
 #define WIFI_MANAGER_H
 
 #include <WiFi.h>
 #include <time.h>
-#include <string.h>
-#include <cstdio>
 
 /**
  * @brief Estabelece a conexão inicial com a rede Wi-Fi.
@@ -22,13 +11,11 @@
  *          ou seja, ela bloqueará a execução do programa até que a conexão seja estabelecida com sucesso.
  *          O progresso da conexão é impresso no monitor serial para fins de depuração. Ao final, o estado da
  *          conexão é refletido no ponteiro `conectado`.
- * @param ssid O nome (SSID) da rede Wi-Fi à qual se deseja conectar.
- * @param password A senha da rede Wi-Fi.
  * @param conectado Um ponteiro para uma variável booleana que será atualizada para `true` quando a conexão
  *                  for bem-sucedida. Isso permite que o estado da conexão seja compartilhado com outras
- *                  partes do sistema.
+ *                  parte do sistema.
  */
-void conectarWiFi(const char* ssid, const char* password, bool* conectado);
+void conectarWiFi(bool* conectado);
 
 /**
  * @brief Verifica e, se necessário, restabelece a conexão com a rede Wi-Fi.
@@ -37,12 +24,10 @@ void conectarWiFi(const char* ssid, const char* password, bool* conectado);
  *          ela tentará se reconectar automaticamente usando as mesmas credenciais (SSID e senha) fornecidas.
  *          Isso garante que o dispositivo possa se recuperar de perdas temporárias de sinal ou reinicializações
  *          do roteador sem a necessidade de uma reinicialização manual.
- * @param ssid O nome (SSID) da rede Wi-Fi.
- * @param password A senha da rede Wi-Fi.
  * @param conectado Um ponteiro para a variável booleana que armazena o estado da conexão. A função atualizará
  *                  este valor para `true` na reconexão ou o manterá como `false` se a tentativa falhar.
  */
-void reconectarWiFi(const char* ssid, const char* password, bool* conectado);
+void reconectarWiFi(bool* conectado);
 
 /**
  * @brief Sincroniza o relógio do dispositivo com um servidor de tempo da rede (NTP).
--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,6 @@
 // --- Configurações da Rede e Servidores ---
 
 // Credenciais da Rede Wi-Fi
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
 
 // Configurações do Broker MQTT
 const char* mqtt_server    = "0bbdda7fb11e4c4795c3e07e3ac1ff60.s1.eu.hivemq.cloud";
@@ -51,7 +49,7 @@ void setup() {
   iniciarSPIFFS();
   configurarSensor(trigPin, echoPin);
 
-  conectarWiFi(ssid, password, &conectado); 
+  conectarWiFi(&conectado); 
   sincronizarHorarioNTP();      
 
   configurarMQTT(mqtt_server, mqtt_port, mqtt_user, mqtt_password);
@@ -82,22 +80,12 @@ void loop() {
     publicarDadosSensor(&conectado);
   }
 
-  /* Modularizar esse trecho */
   if (!conectado) {
-    reconectarWiFi(ssid, password, &conectado);
+    reconectarWiFi(&conectado);
     if (conectado) {
-
-      /* Modularizar esse trecho */
-      char message[128];
-      if (sizeof(ssid) > 108) {
-        ssid = "SSID muito longo";
-      }
-      snprintf(message, sizeof(message), "Reconectado na rede: %s", ssid);
-
-      publishMessage(String(message), "SUCCESS", "sistema/comunicacao/wifi");
-      sincronizarHorarioNTP();  
+      sincronizarHorarioNTP();
       configurarMQTT(mqtt_server, mqtt_port, mqtt_user, mqtt_password);
-      conectarMQTT();  
+      conectarMQTT();
     }
   }
 }
